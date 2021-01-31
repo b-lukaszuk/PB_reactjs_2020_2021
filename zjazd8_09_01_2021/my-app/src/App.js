d67af62e47496b49d502f58cddd8e606b4e1df25
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2021-01-31 20:43:46 +0100

    kosmetyka, dziala sort po taskDesc, dodac filtrowanie po statucie

--- zjazd8_09_01_2021/my-app/src/App.js
+++ zjazd8_09_01_2021/my-app/src/App.js
@@ -27,7 +27,6 @@ class App extends React.Component {
     this.remTaskFromList = this.remTaskFromList.bind(this);
     this.remAllTasks = this.remAllTasks.bind(this);
     this.sortTasks = this.sortTasks.bind(this);
-    this.showTasksByStatus = this.showTasksByStatus.bind(this);
   }
 
   /**
