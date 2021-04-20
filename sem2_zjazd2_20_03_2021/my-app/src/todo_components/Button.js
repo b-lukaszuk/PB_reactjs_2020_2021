9a6cdd9a7c04b5a0b32e129be24695c50c74927f
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2021-04-20 21:56:00 +0200

    utworzenie oddzielnego komponentu Button.js + styli Button.css

---
+++ sem2_zjazd2_20_03_2021/my-app/src/todo_components/Button.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Button.css";
+
+function Button(props) {
+    return (
+        <button className={props.className}
+            onClick={() => props.onClick()}>
+            {props.btnText}
+        </button>
+    );
+}
+
+export default Button;
