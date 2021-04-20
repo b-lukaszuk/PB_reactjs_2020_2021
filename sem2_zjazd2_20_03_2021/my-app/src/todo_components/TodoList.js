d56e86183ef1737d88460533c818dd9302a47783
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2021-04-20 18:56:49 +0200

    przerobka todoList i todoSorter z rozw klasowego na rozw funkcyjne

--- sem2_zjazd2_20_03_2021/my-app/src/todo_components/TodoList.js
+++ sem2_zjazd2_20_03_2021/my-app/src/todo_components/TodoList.js
@@ -2,52 +2,48 @@ import React from "react";
 import "./TodoList.css";
 import TodoItem from "./TodoItem";
 
-class TodoList extends React.Component {
-  constructor(props) {
-    super(props);
-    console.log("TodoList constructor");
-  }
-
-  /**
-   * zwraca komponent TodoItem
-   * @param {Object} item - task postaci {taskDesc: "costam", done: true|false}
-   * @return komponent TodoItem
-   */
-  newListItem(item) {
+function TodoList(props) {
+    const todos = props.todos;
     return (
-      <TodoItem
-        key={item.taskDesc}
-        item={item}
-        toggleDone={this.props.toggleDone}
-        onClickButton={this.props.onClickButton}
-      />
+        <div className="TodoList">
+            <h2>{props.listName}</h2>
+            {/* tabela, celem lepszego wyrownania elementow */}
+            <table>
+                <thead>
+                    <tr>
+                        <th className="taskDesc">task</th>
+                        <th className="status">done?</th>
+                        <th className="remBut">remove from list</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {todos.map((item) => {
+                        return newListItem(item,
+                            props.toggleDone,
+                            props.onClickButton);
+                    })}
+                </tbody>
+            </table>
+            <br />
+        </div>
     );
-  }
-
-  render() {
-    const todos = this.props.todos;
-    return (
-      <div className="TodoList">
-        <h2>{this.props.listName}</h2>
-        {/* tabela, celem lepszego wyrownania elementow */}
-        <table>
-          <thead>
-            <tr>
-              <th className="taskDesc">task</th>
-              <th className="status">done?</th>
-              <th className="remBut">remove from list</th>
-            </tr>
-          </thead>
-          <tbody>
-            {todos.map((item) => {
-              return this.newListItem(item);
-            })}
-          </tbody>
-        </table>
-        <br />
-      </div>
-    );
-  }
 }
 
 export default TodoList;
+
+
+/**
+ * zwraca komponent TodoItem
+ * @param {Object} item - task postaci {taskDesc: "costam", done: true|false}
+ * @return komponent TodoItem
+ */
+function newListItem(item, toggleDone, onClickButton) {
+    return (
+        <TodoItem
+            key={item.taskDesc}
+            item={item}
+            toggleDone={toggleDone}
+            onClickButton={onClickButton}
+        />
+    );
+}
--- sem2_zjazd2_20_03_2021/my-app/src/todo_components/TodoSorter.js
+++ sem2_zjazd2_20_03_2021/my-app/src/todo_components/TodoSorter.js
@@ -1,22 +1,15 @@
 import React from "react";
 
-class TodoSorter extends React.Component {
-  constructor(props) {
-    super(props);
-    console.log("TodoSorter constructor");
-  }
-
-  render() {
+function TodoSorter(props) {
     return (
-      <div>
-        <span>Sort by task description</span>
+        <div>
+            <span>Sort by task description</span>
         &nbsp;
-        <button onClick={() => this.props.onClick()}>
-          {this.props.butMessage}
-        </button>
-      </div>
+            <button onClick={() => props.onClick()}>
+                {props.butMessage}
+            </button>
+        </div>
     );
-  }
 }
 
 export default TodoSorter;
