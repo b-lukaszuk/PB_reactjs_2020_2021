780d9cd18fe35542b464d30e87fff8226276b302
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2021-06-18 17:25:12 +0200

    dodanie reducera dla todosow

--- sem2_zaliczenie/my-app/src/conteners/todosPage/TodosPage.js
+++ sem2_zaliczenie/my-app/src/conteners/todosPage/TodosPage.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useContext } from "react";
+import React, { useEffect, useState, useContext, useReducer } from "react";
 
 import { authContext } from "../../components/AuthProvider";
 import Button from "../../components/Button";
@@ -13,20 +13,62 @@ import {
 function TodosPage() {
     const { logout } = useContext(authContext);
 
+    const todosInitialState = {
+        todos: getKeyFromLocalStorage("todos", []),
+    }
+
+    const todosReducer = (state, action) => {
+        switch (action.type) {
+            case "todosLoad":
+                return { ...state, todos: action.payload };
+            case "todosAddOne":
+                return { ...state, todos: [...state.todos, action.payload] };
+            case "todosRemoveOne":
+                return {
+                    ...state, todos:
+                        state.todos.filter((todo) => {
+                            return todo.id !== action.todoId;
+                        })
+                };
+            case "todosRemoveAll":
+                return { ...state, todos: [] };
+            case "todosToggleCompletedOne":
+                return {
+                    ...state, todos: state.todos.map((todo) => {
+                        if (todo.id === action.todoId) {
+                            return { ...todo, completed: !todo.completed };
+                        } else {
+                            return todo;
+                        }
+                    })
+                };
+            case "todosSortAll":
+                return {
+                    ...state, todos: action.sortAsc ? state.todos.sort(
+                        (t1, t2) => t1.name.localeCompare(t2.name)
+                    ) : state.todos.sort(
+                        (t1, t2) => t2.name.localeCompare(t1.name))
+                };
+            default:
+                return state;
+        }
+    }
+
     const urlForTodos = "https://jsonplaceholder.typicode.com/users/1/todos";
-    const [todos, setTodos] = useState(getKeyFromLocalStorage("todos", []));
-    console.log(todos);
+    const [todosState, todosDispatch] = useReducer(todosReducer, todosInitialState);
 
     useEffect(() => {
         async function fetchData() {
             let response = await fetch(urlForTodos);
             let data = await response.json();
             let modifData = data.map((item) => {
-                return { id: item.id, name: item.title, completed: item.completed };
+                return {
+                    id: item.id, name: item.title, completed: item.completed
+                };
             });
             if (!isKeyInLocalStorage("todos")) {
                 console.log("fetching todos from url");
-                setTodos(modifData);
+                todosDispatch({ type: "todosLoad", payload: modifData });
             } else {
                 console.log("preserving todos from localStorage");
             }
@@ -45,8 +87,8 @@ function TodosPage() {
 
     // pushes todos to localStorage
     useEffect(() => {
-        pushDictToLocalStorage({ todos: todos });
-    }, [todos]);
+        pushDictToLocalStorage({ todos: todosState.todos });
+    }, [todosState]);
 
     // pushes showCompleted to localStorage
     useEffect(() => {
@@ -79,13 +121,8 @@ function TodosPage() {
      * zmienia sortAsc
      */
     const sortTasks = () => {
-        if (sortAsc) {
-            setTodos(todos.sort((t1, t2) => t1.name.localeCompare(t2.name)));
-            setSortAsc(!sortAsc);
-        } else {
-            setTodos(todos.sort((t1, t2) => t2.name.localeCompare(t1.name)));
-            setSortAsc(!sortAsc);
-        }
+        todosDispatch({ type: "todosSortAll", sortAsc: sortAsc });
+        setSortAsc(!sortAsc);
     };
 
     /**
@@ -95,15 +132,7 @@ function TodosPage() {
      * togglujac status completed dla danego obiektu
      */
     const toggleCompleted = (taskId) => {
-        setTodos(
-            todos.map((item) => {
-                if (item.id === taskId) {
-                    return { ...item, completed: !item.completed };
-                } else {
-                    return item;
-                }
-            })
-        );
+        todosDispatch({ type: "todosToggleCompletedOne", todoId: taskId });
     };
 
     /**
@@ -114,13 +143,15 @@ function TodosPage() {
         setTaskToAdd(e.target.value);
     };
 
+    /**
+     * zwraca pierwsze wolne id todosa, max+1
+     * @param {Object[]} todos - todosy z ktorych beda brane id do analizy
+     */
     const getFirstFreeId = (todos) => {
         let usedIds = todos.map((todo) => {
             return todo.id;
         });
-        console.log(usedIds);
         let maxId = usedIds.length === 0 ? 0 : Math.max(...usedIds);
-        console.log(maxId);
         return maxId + 1;
     };
 
@@ -138,13 +169,11 @@ function TodosPage() {
         } else if (!newTaskDesc.trim().match(/[a-zA-Z]+/)) {
             alert("Task title must contain at least 1 alphabetic character");
         } else {
-            let newId = getFirstFreeId(todos);
-            console.log("adding task, new id:", newId);
-            console.log("adding task, name:", newTaskDesc);
-            setTodos([
-                ...todos,
-                { id: newId, name: newTaskDesc.trim(), completed: false },
-            ]);
+            let newId = getFirstFreeId(todosState.todos);
+            todosDispatch({
+                type: "todosAddOne",
+                payload: { id: newId, name: newTaskDesc.trim(), completed: false }
+            });
         }
         setTaskToAdd("");
     };
@@ -153,26 +182,22 @@ function TodosPage() {
      * usuwa dany task z listy todos-ow
      * @param {number} idToRemove - id taska do usuniecia
      * (id sa unikalne w obrebie todos)
-     * zmienia todos
+     * zmienia todosState.todos
      */
     const remTaskFromList = (idToRemove) => {
-        setTodos(
-            todos.filter((item) => {
-                return item.id !== idToRemove;
-            })
-        );
+        todosDispatch({ type: "todosRemoveOne", todoId: idToRemove });
     };
 
     /**
      * usuwa wszystkie taski z listy todos
      */
     const remAllTasks = () => {
-        setTodos([]);
+        todosDispatch({ type: "todosRemoveAll" });
     };
 
     return (
         <div>
-            <span>Welcome to TODO app </span>
+            <span>Welcome to our TODO app! </span>
             <Button
                 className="normalBut"
                 btnText="logout"
@@ -197,7 +222,7 @@ function TodosPage() {
             {showCompleted && (
                 <TodoList
                     listName="Completed tasks:"
-                    todos={todos.filter((t) => {
+                    todos={todosState.todos.filter((t) => {
                         return t.completed;
                     })}
                     toggleCompleted={toggleCompleted}
@@ -208,7 +233,7 @@ function TodosPage() {
             {showPending && (
                 <TodoList
                     listName="Pending tasks:"
-                    todos={todos.filter((t) => {
+                    todos={todosState.todos.filter((t) => {
                         return !t.completed;
                     })}
                     toggleCompleted={toggleCompleted}
